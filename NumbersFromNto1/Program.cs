082e46f6d2b67b96e8896b7cdf40c36c27fad9f6
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 14:40:09 +0300

    Initial commit

---
+++ NumbersFromNto1/Program.cs
@@ -0,0 +1,12 @@
+﻿class NumbersFromNto1
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = N; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
